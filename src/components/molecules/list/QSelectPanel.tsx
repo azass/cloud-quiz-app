f74c50ae1fbb115e0ea95b26864df981bd74d44e
sz <zag61728@google.com>
2024-01-31 16:17:15 +0900

    update

--- src/components/molecules/list/QSelectPanel.tsx
+++ src/components/molecules/list/QSelectPanel.tsx
@@ -30,7 +30,7 @@ export const QSelectPanel: FC = memo(() => {
                 <QListFilter />
               </div>
             </div>
-            <div className="px-6 pb-60">
+            <div className="px-6 pb-80">
               <QList />
             </div>
           </div>
