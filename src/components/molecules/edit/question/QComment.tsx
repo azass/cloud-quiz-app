003728e195abc6f7a473124f0946128fb90c0289
sz <zag61728@google.com>
2024-09-16 10:28:07 +0900

    update

--- src/components/molecules/edit/question/QComment.tsx
+++ src/components/molecules/edit/question/QComment.tsx
@@ -6,14 +6,14 @@ interface Props {
   comment: Comment
 }
 export const QComment: FC<Props> = ({ comment }) => {
-  const {lang} = useLangContext()
+  const { lang } = useLangContext()
   return (
     <li className="py-2">
       <ul>
         <div className={`-mt-5 ${Colors.strong}`}>
           {comment.date && <div>{comment.date}</div>}
           {comment.badge && <div>{comment.badge}</div>}
-          <div className="text-base w-full text-white whitespace-pre-wrap">
+          <div className="text-base w-full text-white whitespace-pre-wrap font-body">
             <p>{comment.comment_jp}</p>
           </div>
         </div>
